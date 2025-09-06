2591fc868eb5cdfad88da69e801c32281ef02ce5
Erik Martino <erik.martino@gmail.com>
2025-09-06 14:20:19 +0200

    feat: list content

--- main.go
+++ main.go
@@ -22,12 +22,6 @@ func main() {
 	// Create S3 client
 	client := s3.NewFromConfig(cfg)
 
-	// Fetch S3 buckets
-	result, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
-	if err != nil {
-		log.Fatalf("failed to list buckets: %v", err)
-	}
-
 	// Create TUI application
 	app := tview.NewApplication()
 	list := tview.NewList()
@@ -35,18 +29,25 @@ func main() {
 		SetTextAlign(tview.AlignCenter).
 		SetText("Select an S3 bucket")
 
-	// Populate the list with S3 buckets
-	for _, bucket := range result.Buckets {
-		bucketName := *bucket.Name
-		list.AddItem(bucketName, "", 0, func() {
-			text.SetText(fmt.Sprintf("s3://%s", bucketName))
-		})
-	}
+	// Fetch S3 buckets and populate the list
+	go func() {
+		result, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+		if err != nil {
+			log.Fatalf("failed to list buckets: %v", err)
+		}
 
-	// Add a "Quit" option
-	list.AddItem("Quit", "", 'q', func() {
-		app.Stop()
-	})
+		app.QueueUpdateDraw(func() {
+			for _, bucket := range result.Buckets {
+				bucketName := *bucket.Name
+				list.AddItem(bucketName, "", 0, func() {
+					text.SetText(fmt.Sprintf("s3://%s", bucketName))
+				})
+			}
+			list.AddItem("Quit", "", 'q', func() {
+				app.Stop()
+			})
+		})
+	}()
 
 	// Hide secondary text to remove blank lines
 	list.ShowSecondaryText(false)
@@ -56,6 +57,54 @@ func main() {
 		SetDirection(tview.FlexRow).
 		AddItem(text, 3, 1, false).
 		AddItem(list, 0, 1, true)
+
+	// Function to list objects in a bucket
+	listObjects := func(bucketName string) {
+		text.SetText(fmt.Sprintf("s3://%s", bucketName))
+		objectList := tview.NewList()
+
+		objectList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			if event.Key() == tcell.KeyLeft {
+				app.SetRoot(flex, true)
+			}
+			return event
+		})
+
+		objectFlex := tview.NewFlex().
+			SetDirection(tview.FlexRow).
+			AddItem(text, 3, 1, false).
+			AddItem(objectList, 0, 1, true)
+
+		app.SetRoot(objectFlex, true)
+
+		// Fetch objects from the selected bucket
+		go func() {
+			objects, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+				Bucket: &bucketName,
+			})
+			if err != nil {
+				log.Printf("failed to list objects: %v", err)
+				return
+			}
+
+			app.QueueUpdateDraw(func() {
+				for _, object := range objects.Contents {
+					objectList.AddItem(*object.Key, "", 0, nil)
+				}
+			})
+		}()
+	}
+
+	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEnter || event.Key() == tcell.KeyRight {
+			selectedItem := list.GetCurrentItem()
+			if selectedItem >= 0 && selectedItem < list.GetItemCount()-1 {
+				bucketName, _ := list.GetItemText(selectedItem)
+				listObjects(bucketName)
+			}
+		}
+		return event
+	})
 
 	// Keybindings
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
